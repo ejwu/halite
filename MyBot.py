2fca16907bce0fbd61dfb4e931461f45274fdd93
ejwu <eric.j.wu@gmail.com>
2017-10-26 02:00:19 -0700

    [Settler 4] Gotta go fast

--- MyBot.py
+++ MyBot.py
@@ -19,7 +19,7 @@ from collections import Counter
 
 # GAME START
 # Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
-game = hlt.Game("Settler 3")
+game = hlt.Game("Settler 4")
 # Then we print our start message to the logs
 logging.info("Starting my Settler bot!")
 
@@ -70,7 +70,7 @@ while True:
                 navigate_command = ship.navigate(
                     ship.closest_point_to(planet),
                     game_map,
-                    speed=int(hlt.constants.MAX_SPEED/2),
+                    speed=int(hlt.constants.MAX_SPEED),
                     ignore_ships=True)
                 # If the move is possible, add it to the command_queue (if there are too many obstacles on the way
                 # or we are trapped (or we reached our destination!), navigate_command will return null;
