daf9cb17ba87c4d8e7ea14c72034a643c95706b5
ejwu <eric.j.wu@gmail.com>
2017-11-01 17:49:30 -0700

    Add .sh versions of .bat viles

--- gym.bat
+++ gym.bat
@@ -1,2 +1,2 @@
  .\hlt_client\client.py gym -r "python MyBot.py" -r "python bot_LA_6.py" -b "halite" -H 160 -W 240 -i 20
- mv replay* replays/
+ mv replay*.hlt replays/
---
+++ gym.sh
@@ -0,0 +1,2 @@
+ ./hlt_client/client.py gym -r "python3 MyBot.py" -r "python3 bot_LA_6.py" -b "./halite" -H 160 -W 240 -i 20
+ mv replay*.hlt replays/
